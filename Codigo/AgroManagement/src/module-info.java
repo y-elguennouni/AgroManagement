ab492a8734e3c394d39b4a00f202479425ab4782
yuelguennouni <luxemborgo31@hotmail.com>
2019-03-30 17:21:36 +0100

    Create new module

---
+++ Codigo/AgroManagement/src/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author Youness
+ *
+ */
+module agroManagement {
+	exports Vista;
+	exports Controlador;
+	exports Modelo;
+	exports Main;
+
+	requires agroManagement;
+	requires java.desktop;
+	
+}
